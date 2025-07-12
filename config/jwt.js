6b90105470a457139ff401cce9d305ff381afc30
aravindrajacoder <aravind@oceansoftteam.in>
2025-07-12 14:32:33 +0530

    Token Expires removed

--- config/jwt.js
+++ config/jwt.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key-here';
-const JWT_EXPIRES_IN = '750h';
+const JWT_EXPIRES_IN = '';
 
 const generateToken = (donorId) => {
   return jwt.sign(
     { donorId },
     JWT_SECRET,
-    { expiresIn: JWT_EXPIRES_IN }
+    // { expiresIn: JWT_EXPIRES_IN }
   );
 };
 
 module.exports = {
   generateToken,
   JWT_SECRET,
-  JWT_EXPIRES_IN
+ // JWT_EXPIRES_IN
 };
